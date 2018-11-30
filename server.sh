e0c82f6b0e935c028a877aaea17b3fe7f2d327e4
Hyunwoo Jung <chaosphch@gmail.com>
2018-11-30 19:36:13 +0900

    Create server demonizing script

---
+++ server.sh
@@ -0,0 +1,22 @@
+# Put your server-running command here
+runserver_command="sleep 100"
+
+# The pid of the running server is saved in $pidfilename
+# Using an absolute path is recommanded
+pidfilename=pid
+
+case $1 in
+  run)
+    (eval "$runserver_command &" && echo $! > $pidfilename)
+    ;;
+  stop)
+    # Kill the server with pid
+    kill -9 $(cat pid)
+    # Kill all child processes
+    pkill -9 -P $(cat pid)
+    rm $pidfilename
+    ;;
+  *)
+    echo 'server.sh doesn'\''t support the sub-command '\"$1\"
+    echo 'usage: "sh server.sh run" or "sh .server stop'
+esac
